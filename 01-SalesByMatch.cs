65a4b903b14dec441eb53287a7b4a44effd656b8
Saqib Amin <saqibamin257@gmail.com>
2021-07-06 06:00:46 -0700

    SalesByMatch updated code

--- 01-SalesByMatch.cs
+++ 01-SalesByMatch.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace HackerRank
 {
-   public class Result
+   public class SalesByMatch_Result
     {       
         public static int sockMerchant(int n, List<int> ar) 
         {
@@ -30,14 +30,24 @@ namespace HackerRank
         }
     }
 
-   public class Solution 
+   public class SalesByMatch_Solution
     {
         public static void SalesByMatch()
         {
+            //TestCase-1
             int n = 9;
             string numbers = "10 20 20 10 10 30 50 10 20";
+            //output=>3
+
+
+            //TestCase-2
+            //int n = 10;           
+            //string numbers = "1 1 3 1 2 1 3 3 3 3";
+            //output=>4
+
+
             List<int> ar = numbers.TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
-            int result = Result.sockMerchant(n, ar);
+            int result = SalesByMatch_Result.sockMerchant(n, ar);
             Console.WriteLine("Result:" + result);
             Console.ReadKey(true);
         }        
--- Program.cs
+++ Program.cs
@@ -11,9 +11,10 @@ namespace HackerRank
             #region Prob-1:SalesByMatch
             //Problem-1:SalesByMatch
             //Please See Q1 pdf for question details.
-
-            Solution.SalesByMatch();            
+            SalesByMatch_Solution.SalesByMatch();
             #endregion
+
+            
 
         }
     }
