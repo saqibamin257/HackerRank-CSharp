b3bcd519869ab9e492982b146a1fd885eefdc41e
Saqib Amin <saqibamin257@gmail.com>
2021-07-06 06:17:26 -0700

    02-DrawingBook challenge

---
+++ 02-DrawingBook.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank
+{
+   public class DrawingBook_Result
+    {
+        /*
+     * Complete the 'pageCount' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER p
+     */
+
+        public static int pageCount(int n, int p)
+        {
+            if (n % 2 == 0)
+                n = n + 1;
+
+            List<int> PageList = new List<int>();
+            int left = 0, right = 0, min = 0, count = -1;
+            for (int a = 0; a <= n; a++)
+            {
+                PageList.Add(a);
+            }
+
+            //from left
+            for (int i = 0; i <= n; i += 2)
+            {
+                count++;
+                if (PageList[i] == p || PageList[i + 1] == p)
+                {
+                    left = count;
+                    break;
+                }
+            }
+
+            //from right
+            count = -1;
+            for (int j = n; j >= 0; j -= 2)
+            {
+                count++;
+                if (PageList[j] == p || PageList[j - 1] == p)
+                {
+                    right = count;
+                    break;
+                }
+
+            }
+            //calculate minimum.
+            if (left <= right)
+                min = left;
+            else
+                min = right;
+
+            return min;
+        }
+
+    }
+    public class DrawingBook_Solution 
+    {
+        public static void DrawingBook() 
+        {
+            //TestCase -1
+            //int n = 3, p = 3;
+            //output(left=1,right=1) => min=1
+
+            //TestCase -2
+            //int n = 6, p = 2;
+            //output(left=1,right=2) => min=
+
+            //TestCase -3
+            int n = 5, p = 4;
+            //output(left=2,right=0) => min=0
+
+            int result = DrawingBook_Result.pageCount(n, p);
+            Console.WriteLine(result);
+        }
+        
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,10 +11,17 @@ namespace HackerRank
             #region Prob-1:SalesByMatch
             //Problem-1:SalesByMatch
             //Please See Q1 pdf for question details.
-            SalesByMatch_Solution.SalesByMatch();
+            //SalesByMatch_Solution.SalesByMatch();
             #endregion
 
-            
+
+            #region Prob-1:DrawingBook
+            //Problem-1:DrawingBook
+            //Please See Q2 pdf for question details.
+            DrawingBook_Solution.DrawingBook();
+            #endregion
+
+
 
         }
     }
